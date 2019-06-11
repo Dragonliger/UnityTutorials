ee810a1b1e4378b695860d5e7ca42cf395a9435b
Alex Castelan <jalejandrocastelani@gmail.com>
2019-06-10 20:02:08 -0700

    Tutorial 1 - Attributes
    Challenge script and Solution script

---
+++ Tutorial 1 - Attributes/ChallengeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeScript : MonoBehaviour
+{
+    // Public parameters
+    public Texture2D FireColor;
+    public Texture2D FireMetallic;
+    public Texture2D FireNormalMap;
+    public Texture2D FireEmission;
+    public Color HDRcolorForOtherScript;
+    public Texture2D WaterColor;
+    public Texture2D waterMetallic;
+    public Texture2D WaterNormalMap;
+    public Texture2D WaterEmission;
+    //Controls the water to fire ratio
+    public float WaterPercentage = 50.0f;
+
+    // Private parameters
+    private Animation ClickedAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClickedAnim = gameObject.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
---
+++ Tutorial 1 - Attributes/SolutionScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class SolutionScript : MonoBehaviour
+{
+    // Public parameters
+    [Header("Fire Textures")]
+    public Texture2D FireColor;
+    public Texture2D FireMetallic;
+    public Texture2D FireNormalMap;
+    public Texture2D FireEmission;
+    [HideInInspector]
+    public Color HDRcolorForOtherScript;
+    [Header("Water Textures")]
+    public Texture2D WaterColor;
+    public Texture2D waterMetallic;
+    public Texture2D WaterNormalMap;
+    public Texture2D WaterEmission;
+    [Range(0,100)][Tooltip("Controls the fire to water ratio")]
+    public float WaterPercentage = 50.0f;
+
+    // Private parameters
+    private Animation ClickedAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClickedAnim = gameObject.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
